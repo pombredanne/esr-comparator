e160fc573dea094c9f55f3612aa5c8b71ecdfe81
Eric S. Raymond <esr@thyrsus.com>
2003-09-11 21:11:00 +0000

    No more padding dependency.

--- shred.h
+++ shred.h
@@ -4,19 +4,14 @@
 
 /*
  * 65,536 lines should be enough, but maybe someday this will be 32 bits.
- * The point of making it a short is that the data sets can get quite large.
+ * The point of making it a short is that the data sets can get quite large,
+ * so we want the hash_t and sorthash_t structures to be small.
  */
 typedef u_int16_t	linenum_t;
 #define TONET		htons
 #define FROMNET		ntohs
-#define UNIQUE_FLAG	(linenum_t)-1
+#define UNIQUE_FLAG	(linenum_t)-1	/* 2s-complement assumption */
 
-/*
- * The code depends on the assumption that this structure has 
- * no leading, trailing, or internal padding. This should be the 
- * case on all modern 8-bit-byte, byte-addressible machines,
- * whether 32 or 64 bit.
- */
 #define HASHSIZE	16
 struct hash_t
 {
